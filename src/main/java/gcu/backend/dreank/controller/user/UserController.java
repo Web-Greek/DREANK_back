3521dd49753e930560a36bfb2dff45c6235bdd3a
ejsong25 <ejsong25@gmail.com>
2024-05-27 00:41:24 +0900

    개인정보 수정 initial commit

--- src/main/java/gcu/backend/dreank/controller/user/UserController.java
+++ src/main/java/gcu/backend/dreank/controller/user/UserController.java
@@ -1,9 +1,11 @@
 package gcu.backend.dreank.controller.user;
 
+import gcu.backend.dreank.dto.request.login.SessionConst;
+import gcu.backend.dreank.dto.request.login.SessionInfo;
+import gcu.backend.dreank.dto.request.user.UserCreateRequest;
+import gcu.backend.dreank.dto.request.user.UserUpdateRequest;
 import gcu.backend.dreank.service.user.UserService;
-import gcu.backend.dreank.dto.request.UserCreateRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,10 +20,11 @@ public class UserController {
     }
 
 //    UPDATE- 정보 수정
-    //@PutMapping
-    //public void updateUser(@RequestBody UserUpdateRequest request){
-
-    //}
+    @PatchMapping("/user/update")
+    public void updateUser(
+            @RequestBody UserUpdateRequest request, @SessionAttribute(SessionConst.LOGIN_MEMBER) SessionInfo sessionInfo){
+        userService.updateUser(request, sessionInfo.getId());
+    }
 
 //    DELETE
 //    탈퇴하기
